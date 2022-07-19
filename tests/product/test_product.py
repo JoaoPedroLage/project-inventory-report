c4defec26978b4b94598b79dad93c9c421be9e91
JoaoPedroLage <joaopedro.lage@outlook.com>
2022-07-19 10:33:03 -0300

    feat: make the test for the constructor of Product object

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'best product',
+        'mindset company',
+        '01/07/2022',
+        '01/07/2023',
+        '0123456789',
+        'store in a cool place, away from the sun'
+    )
+
+    slice01 = f"O produto {product.nome_do_produto}"
+    slice02 = f" fabricado em {product.data_de_fabricacao}"
+    slice03 = f" por {product.nome_da_empresa} com validade"
+    slice04 = f" até {product.data_de_validade}"
+    slice05 = f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+
+    string = slice01 + slice02 + slice03 + slice04 + slice05
+
+    assert str(product) == string
