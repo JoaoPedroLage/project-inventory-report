d65b8e520976916bd443ffacfa06c84aeb99e676
JoaoPedroLage <joaopedro.lage@outlook.com>
2022-07-28 18:39:35 -0300

    feat: fill main function in module Inventory_report/main.py

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,19 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import sys
+
+
 def main():
-    pass
+    if (len(sys.argv) < 3):
+        return sys.stderr.write("Verifique os argumentos\n")
+    else:
+        if sys.argv[1].endswith(".csv"):
+            report = InventoryRefactor(CsvImporter)
+        if sys.argv[1].endswith(".json"):
+            report = InventoryRefactor(JsonImporter)
+        if sys.argv[1].endswith(".xml"):
+            report = InventoryRefactor(XmlImporter)
+
+    return sys.stdout.write(report.import_data(sys.argv[1], sys.argv[2]))
